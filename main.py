8f66c1a859d8305c7a4a3529837309da8165705d
pheni-18 <pheni.engineer@gmail.com>
2021-07-23 10:40:52 +0900

    Extend open api

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from presentation.controllers import router
 
 
 api = FastAPI()
 
 api.include_router(router)
+
+
+def custom_openapi():
+    if api.openapi_schema:
+        return api.openapi_schema
+
+    openapi_schema = get_openapi(
+        title='Onion arch practice',
+        version='1.0.0',
+        description='This is a practice of ddd and onion arch.',
+        routes=api.routes,
+    )
+    openapi_schema['info']['x-logo'] = {
+        'url': 'https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png',
+    }
+    api.openapi_schema = openapi_schema
+    return api.openapi_schema
+
+
+api.openapi = custom_openapi
--- presentation/controllers/user.py
+++ presentation/controllers/user.py
@@ -1,9 +1,10 @@
 from ..converters import UserConverter
-from ..schemas import UserCreate, UserUpdate
+from ..schemas import UserCreate, UserUpdate, User
 from depends_provider import DependsProvider
 from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 from typing import ClassVar
+from typing import List
 
 import application.services as app_services
 
@@ -29,29 +30,54 @@ class UserController:
         self._app_user_service = app_user_service
         self._user_converter = user_converter
 
-    @router.get(_prefix + '/')
+    @router.get(
+        _prefix + '/',
+        response_model=List[User]
+    )
     async def get_users(self):
         user_dtos = self._app_user_service.get_all()
         return [self._user_converter.to_schema(user_dto) for user_dto in user_dtos]
 
-    @router.get(_prefix + '/{id}')
+    @router.get(
+        _prefix + '/{id}',
+        response_model=User,
+        responses={
+            404: {"model": User, "description": "The item was not found"},
+        },
+    )
     async def get_user(self, id: str):
         user_dto = self._app_user_service.get(id)
         return self._user_converter.to_schema(user_dto)
 
-    @router.post(_prefix + '/')
+    @router.post(
+        _prefix + '/',
+        status_code=201,
+        response_model=User,
+    )
     async def create_user(self, user_create: UserCreate):
         user_create_dto = self._user_converter.to_create_dto(user_create)
         user_dto = self._app_user_service.register(user_create_dto)
         return self._user_converter.to_schema(user_dto)
 
-    @router.patch(_prefix + '/{id}')
+    @router.patch(
+        _prefix + '/{id}',
+        response_model=User,
+        responses={
+            404: {"model": User, "description": "The item was not found"},
+        },
+    )
     async def update_user(self, id: str, user_update: UserUpdate):
         user_update_dto = self._user_converter.to_update_dto(id, user_update)
         user_dto = self._app_user_service.update(user_update_dto)
         return self._user_converter.to_schema(user_dto)
 
-    @router.delete(_prefix + '/{id}')
+    @router.delete(
+        _prefix + '/{id}',
+        status_code=204,
+        responses={
+            404: {"model": User, "description": "The item was not found"},
+        },
+    )
     async def delete_user(self, id: str):
         self._app_user_service.delete(id)
         return
